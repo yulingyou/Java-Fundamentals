46e509b9d53b8e104f60f50d6556d48b70a7dd94
Daisy <ysiad926@gmail.com>
2024-03-11 15:25:26 +0000

    JavaQuiz update

--- bites/examples/JavaQuiz.java
+++ bites/examples/JavaQuiz.java
@@ -2,6 +2,10 @@ package bites.examples;
 
 public class JavaQuiz {
     public static void main(String[] args){
-        System.out.println("1. Yes, 2. Converted to lower code, 3. It is executed automatically and you need one per program(no more, no less) , 4. In the context of a method signature, it means the method is not intended to return anything");
+        System.out.println(
+                        "1. Does all Java code have to be inside a class? Yes, " +
+                        "2. What is compilation?Converted to lower code, " +
+                        "3. What is special about the main method? It is executed automatically and you need one per program(no more, no less) , " +
+                        "4. What does void mean? In the context of a method signature, it means the method is not intended to return anything");
     }
 }
