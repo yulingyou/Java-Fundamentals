5dd6ee81579dea3097424ed77114f63c2a6aefb3
Daisy <ysiad926@gmail.com>
2024-03-11 14:54:01 +0000

    Writing Java Exercise & Challenge

---
+++ bites/examples/JavaChallenge.java
@@ -0,0 +1,8 @@
+package bites.examples;
+
+public class JavaChallenge {
+    public static void main(String[] args){
+        System.out.println("Hello Challenge");
+        System.out.println("Hi Challenge");
+    }
+}
---
+++ bites/examples/JavaQuiz.java
@@ -0,0 +1,7 @@
+package bites.examples;
+
+public class JavaQuiz {
+    public static void main(String[] args){
+        System.out.println("1. Yes, 2. Converted to lower code, 3. It is executed automatically and you need one per program(no more, no less) , 4. In the context of a method signature, it means the method is not intended to return anything");
+    }
+}
