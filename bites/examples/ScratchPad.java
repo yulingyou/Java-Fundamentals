3214e2474494a595cbefebdb219027c374bbfc4f
Daisy <ysiad926@gmail.com>
2024-03-14 16:42:01 +0000

    HashMap exercise 2

--- bites/examples/ScratchPad.java
+++ bites/examples/ScratchPad.java
@@ -4,14 +4,14 @@ import java.util.HashMap;
 
 public class ScratchPad {
     public static void main(String[] args) {
-        HashMap<String, Integer> scores = new HashMap<String, Integer>();
+        HashMap<String, Integer[]> scores = new HashMap<String, Integer[]>();
 
-        scores.put("team 1", 5);
-        scores.put("team 2", 11);
-        scores.put("team 3", 8);
+        scores.put("team 1", new Integer[] {1,3,5});
+        scores.put("team 2",  new Integer[] {7,3,5});
+        scores.put("team 3",  new Integer[] {5,7,0});
 
-        System.out.println(scores.get("team 1"));
-        System.out.println(scores.get("team 2"));
-        System.out.println(scores.get("team 3"));
+        System.out.println(scores.get("team 1")[0]);
+        System.out.println(scores.get("team 2")[1]);
+        System.out.println(scores.get("team 3")[2]);
     }
 }
